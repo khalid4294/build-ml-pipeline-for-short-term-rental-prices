2d9cbf79ffedbbc78756ce695341b1d2d9a8ed89
khalid4294 <alolayankhalid@gmail.com.com>
2023-10-08 22:44:09 +0400

    bug fixe - added logitude validation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,11 @@ def go(args):
     df["last_review"] = pd.to_datetime(df["last_review"])
     logger.info("last_review column converted to datetime")
 
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    df.to_csv("clean_sample.csv")
+
     artifact = wandb.Artifact(
         args.output_artifact,
         type=args.output_type,
